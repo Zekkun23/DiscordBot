a853dd6e0bd1401b3649ef218bfa31aeae771a5b
Zekkun23 <71208147+Zekkun23@users.noreply.github.com>
2020-12-09 13:35:43 +0400

    Add files via upload

--- commands/pw.js
+++ commands/pw.js
@@ -1,18 +1,19 @@
-const {MessageEmbed} = require('discord.js')
 module.exports={
     name: "pw",
     description: "announce passwords",
-    run: async(bot,message,args)=>{
-        let rChannel = message.guild.channels.cache.her(args[0])
-        if(!rChannel)return message.channel.send(`Please specify the channel!`)
-        console.log(rChannel)
-        let MSG = message.content.split(`${bot.prefix}announce ${rChannel.id} `).join("")
-        if(!MSG)return message.channel.send(`Please specify the password!`)
-        const _ = new MessageEmbed()
-        .setTitle(`Password!`)
-        .setDescription(`${MSG}`)
-        .setColor('RANDOM')
-        rChannel.send(_)
+
+    async run (client,message,args) {
+        let msg;
+        let textChannel = message.mentions.channels.first()
         message.delete()
+
+        if(textChannel){
+            msg = args.slice(1).join(" ");
+            textChannel.send(msg)
+        } else{
+            msg = args.join(" ");
+            message.channel.send(msg)
+        }
     }
+ 
 }
