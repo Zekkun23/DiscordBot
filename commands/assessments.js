a031ec3750392542c64f0ac7e8334e5b79716605
Zeggy Fuentes <71208147+Zekkun23@users.noreply.github.com>
2021-01-03 12:52:36 +0400

    Update assessments.js

--- commands/assessments.js
+++ commands/assessments.js
@@ -17,7 +17,7 @@ const embed = {
   "fields": [
     {
       "name": ":floppy_disk: Code Lab I",
-      "value": "**[JAN 10] - Chapter 6 & 7**\nRepository + PowerPoint slides \n• Ch. 6 - Arrays \n• Ch. 7 - Functions \n\n**[JAN 26] - Utility App**\nWord Document\n1,000 words min.\n• Source code file\n• Lots of explanation\n• System flowchart\n• (optional) pseudocode\n\n:paperclip:  Details: https://drive.google.com/file/d/10jD24R21-7v25DSBc8p7xU83yqZVrQqH/view?usp=sharing\n\n"
+      "value": "**[JAN 10] - Chapter 6 & 7**\nRepository + PowerPoint slides \n• Ch. 6 - Arrays \n• Ch. 7 - Functions \n\n**[JAN 26] - Utility App**\nWord Document\n1,000 words min.\n• Source code file\n• Lots of explanation\n• System flowchart\n• (optional) pseudocode\n- [OPTIONAL] Explanatory Video\n3-5 mins length\n• Screen-recorded vid going through your vending machine app\n• Explain the code and methods you used\n• Not graded, but it's practice for upcoming years when we have to do this often\n• Best works are uploaded on BSU UAE Official YT channel\n\n:paperclip:  Details: https://drive.google.com/file/d/10jD24R21-7v25DSBc8p7xU83yqZVrQqH/view?usp=sharing\n\n"
     },
     {
       "name": ":desktop: (BSc) Intro to Computing",
@@ -29,7 +29,7 @@ const embed = {
     },
     {
       "name": ":book: Digital Storytelling",
-      "value": "**[DEC 12] - Portfolio Submissions: Google Drive Link**\n• Photoshop - selective coloration/decoloration, image blend and coloration\n• 30-sec timelapse\n• Advertisement storyboard\n\n**[JAN 30] - Sway Portfolio: Finished Sway Link **\n+ Word Document with essay that must include:\n• Digistory Experience (Video)\n• Twine story\n• 30-sec timelapse\n• Photoshop - selective coloration/decoloration, image blend and coloration\n• Advertisement storyboard\n• Photography works\n• Stop Motion\n\n:paperclip:  Details: https://docs.google.com/document/d/1ph0hcKs3oEWy9k13C3tcXtspLJMqs2UXF_AUd4codsg/edit\n:paperclip:  Sample: https://sway.office.com/DGdGGsAvgZY5uJwa?ref=Link\n"
+      "value": "**[JAN 30] - Sway Portfolio: Finished Sway Link **\n+ Word Document with essay that must include:\n• Digistory Experience (Video)\n• Twine story\n• 30-sec timelapse\n• Photoshop - selective coloration/decoloration, image blend and coloration\n• Advertisement storyboard\n• Photography works\n• Stop Motion\n• Bitsy Game\n\n:paperclip:  Details: https://docs.google.com/document/d/1ph0hcKs3oEWy9k13C3tcXtspLJMqs2UXF_AUd4codsg/edit\n:paperclip:  Sample: https://sway.office.com/DGdGGsAvgZY5uJwa?ref=Link\n"
     }
   ]
 };
