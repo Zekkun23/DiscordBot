ed19d32fbd62c049d9957146981bb1c094f519a0
Zeggy Fuentes <71208147+Zekkun23@users.noreply.github.com>
2021-01-22 00:32:34 +0400

    Update assessments.js

--- commands/assessments.js
+++ commands/assessments.js
@@ -17,11 +17,11 @@ const embed = {
   "fields": [
     {
       "name": ":floppy_disk: Code Lab I",
-      "value": "**[JAN 10] - Chapter 6 & 7**\nRepository + PowerPoint slides \n• Ch. 6 - Arrays \n• Ch. 7 - Functions \n\n**[JAN 26] - Utility App**\nWord Document\n1,000 words min.\n• Source code file\n• Lots of explanation\n• System flowchart\n• (optional) pseudocode\n**[OPTIONAL]** Explanatory Video\n• 3-5 mins length\n• Screen-recorded vid going through your vending machine app\n• Explain the code and methods you used\n• Not graded, but it's practice for upcoming years when we have to do this often\n• Best works are uploaded on BSU UAE Official YT channel\n\n:paperclip:  Details: https://drive.google.com/file/d/10jD24R21-7v25DSBc8p7xU83yqZVrQqH/view?usp=sharing\n\n"
+      "value": "**[JAN 26] - Utility App**\nWord Document\n1,000 words min.\n• Source code file\n• Lots of explanation\n• System flowchart\n• (optional) pseudocode\n**[OPTIONAL]** Explanatory Video\n• 3-5 mins length\n• Screen-recorded vid going through your vending machine app\n• Explain the code and methods you used\n• Not graded, but it's practice for upcoming years when we have to do this often\n• Best works are uploaded on BSU UAE Official YT channel\n\n:paperclip:  Details: https://drive.google.com/file/d/10jD24R21-7v25DSBc8p7xU83yqZVrQqH/view?usp=sharing\n\n"
     },
     {
       "name": ":desktop: (BSc) Intro to Computing",
-      "value": "**[JAN 17] - Research Project: Word Document**\n2,000 words min.\n• Technology Convergence\n\n:paperclip:  Details: https://drive.google.com/file/d/1onbu5fjk6_dcYsOe3sOstcJri7NfYtGZ/view?usp=sharing\n:paperclip:  Sample: https://drive.google.com/file/d/1GpJtPqiP_daEqok4dBGrG6kacFVJEVRp/view?usp=sharing\n\n"
+      "value": "**N/A**"
     },
     {
       "name": ":desktop: (DCC) Fundamentals of Computing",
