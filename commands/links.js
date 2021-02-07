5e3c66d092568db85ae142ce521a285cba1596c8
Zeggy Fuentes <71208147+Zekkun23@users.noreply.github.com>
2021-02-07 07:41:43 +0400

    Update links.js

--- commands/links.js
+++ commands/links.js
@@ -4,16 +4,16 @@ module.exports = {
     execute(message, args){
         const embed = {
             "description": "Links For The Classes",
-            "color": 13632027,
+            "color": 3066993,
             "footer": {
               "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
             },
             "thumbnail": {
-              "url": "https://image.winudf.com/v2/image1/Y29tLmF2YXlhLnNwYWNlc19pY29uXzE1OTQ5NzM3NDdfMDE4/icon.png?w=170&fakeurl=1"
+              "url": "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Google_Meet.max-1100x1100.png"
             },
             "author": {
               "name": "Class Links",
-              "icon_url": "https://image.winudf.com/v2/image1/Y29tLmF2YXlhLnNwYWNlc19pY29uXzE1OTQ5NzM3NDdfMDE4/icon.png?w=170&fakeurl=1"
+              "icon_url": "https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Google_Meet.max-1100x1100.png"
             },
             "fields": [
               {
