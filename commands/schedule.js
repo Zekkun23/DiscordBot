de23afca4d4499d61e377afe033c2205c5fa55ff
Zeggy Fuentes <71208147+Zekkun23@users.noreply.github.com>
2021-10-24 09:29:17 +0400

    Update schedule.js

--- commands/schedule.js
+++ commands/schedule.js
@@ -25,7 +25,7 @@ module.exports = {
               },
               {
                 "name": "__**Monday-Thurs:**__",
-                "value": "[Games Development (CCO5100-20)](https://meet.google.com/nkd-nczg-wuk) [10:00 - 11:00]\n\n[Smartphone Apps](https://meet.google.com/rwe-bmvh-ipq) [11:15 - 12:15]\n\n[Code Lab II](https://meet.google.com/wqq-jwmg-jrt)[12:30 - 1:30]"
+                "value": "[Games Development (CCO5100-20)](https://meet.google.com/nkd-nczg-wuk) [9:30 - 11:00]\n\n[Smartphone Apps](https://meet.google.com/rwe-bmvh-ipq) [11:00 - 12:30]\n\n[Code Lab II](https://meet.google.com/wqq-jwmg-jrt)[1:00 - 2:30]"
               }
             ]
           };
